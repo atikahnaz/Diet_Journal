5a5dd029dfea69d5c62b60ecd2d756460d135c43
Atikah <atikahnazir6@gmail.com>
2023-10-26 17:29:46 +0800

    change date format

--- src/components/DateSelect.jsx
+++ src/components/DateSelect.jsx
@@ -2,13 +2,16 @@ import { useState } from "react";
 import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { DatePicker } from "@mui/x-date-pickers/DatePicker";
+import { format } from "date-fns";
 
 export default function DateSelected({ callbackdate }) {
   const [date, setDate] = useState(null);
   // when onChange, setDate use callbackdate()
   const dateChange = (event) => {
     setDate(event);
-    callbackdate(event);
+    const formattedDate = format(event, "dd/MM/yyyy");
+    callbackdate(formattedDate);
+    console.log(formattedDate);
   };
 
   return (
