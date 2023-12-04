19495817e381f1bfa1e7e65161edeb425093b75c
Atikah <atikahnazir6@gmail.com>
2023-12-04 16:54:44 +0800

    remove .

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "./Diet_Journal/",
+  base: "/Diet_Journal/",
   plugins: [react()],
   build: {
     chunkSizeWarningLimit: 1000, // Adjust this limit based on your needs
