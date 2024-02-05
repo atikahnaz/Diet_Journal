3f3253d3adc6a94cce7efda89063bf3584c929e6
Atikah <atikahnazir6@gmail.com>
2024-02-05 15:22:19 +0800

    remove menu

--- src/components/BarApp.jsx
+++ src/components/BarApp.jsx
@@ -126,12 +126,12 @@ function ResponsiveAppBar() {
             ))}
           </Box>
 
-          <Box sx={{ flexGrow: 0 }}>
-            {/* <Tooltip title="Open settings">
+          {/* <Box sx={{ flexGrow: 0 }}>
+             <Tooltip title="Open settings">
               <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
                 <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
               </IconButton>
-            </Tooltip> */}
+            </Tooltip> 
             <Menu
               sx={{ mt: "45px" }}
               id="menu-appbar"
@@ -154,7 +154,7 @@ function ResponsiveAppBar() {
                 </MenuItem>
               ))}
             </Menu>
-          </Box>
+          </Box> */}
         </Toolbar>
       </Container>
     </AppBar>
