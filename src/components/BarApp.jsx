e54cc9f4388120c8564a9fea507c76e0eb15bd1a
Atikah <atikahnazir6@gmail.com>
2024-02-05 15:19:06 +0800

    remove avatar

--- src/components/BarApp.jsx
+++ src/components/BarApp.jsx
@@ -127,11 +127,11 @@ function ResponsiveAppBar() {
           </Box>
 
           <Box sx={{ flexGrow: 0 }}>
-            <Tooltip title="Open settings">
+            {/* <Tooltip title="Open settings">
               <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
                 <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
               </IconButton>
-            </Tooltip>
+            </Tooltip> */}
             <Menu
               sx={{ mt: "45px" }}
               id="menu-appbar"
